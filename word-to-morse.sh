213ad20f815d7d0a36120ff89af8941ae3d42247
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2026-08-26 14:52:30 -0500

    update

--- word-to-morse.sh
+++ word-to-morse.sh
@@ -16,7 +16,7 @@ echo "$phrase _" |
 ssh lunchbox venv/bin/python juniper-spelling-bee/word-to-morse.py
 
 scp "lunchbox:morse_432hz_$phrase _.wav" ../storage/downloads/
-ssh lunchbox rm -v "morse_432hz_$phrase _.wav"
+ssh lunchbox rm -v "\"morse_432hz_$phrase _.wav\""
 
 #env -C ../storage/downloads python "$(readlink -f "$PWD")/word-to-morse.py" <<< "$phrase _"
 done
